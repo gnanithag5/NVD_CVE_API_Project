c2135bcc5346d35a7f51770cbe88548af1381ac6
Gnanitha Garikipati <gnanithagarikipati@gmail.com>
2025-03-01 09:30:13 -0700

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-# Create an instance of FastAPI
-app = FastAPI()
-
-# Define a simple route
-@app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI!"}
